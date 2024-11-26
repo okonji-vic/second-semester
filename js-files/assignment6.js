2675a1a5bb7df43dddc5a7429e5f43b11ca53a09
Okonji Emeka <65029583+okonji-vic@users.noreply.github.com>
2024-11-27 00:55:57 +0100

    up

---
+++ js-files/assignment6.js
@@ -0,0 +1,13 @@
+// let main = document.querySelector('main')
+// let div = document.querySelector('.div-section3-child1')
+// let body = document.querySelector('body')
+// let color = ['darkgrey', 'blue', 'green', 'dark', 'purple', 'orange', 'pink', 'brown', 'black', 'white', 'red', 'yellow', 'lightblue', 'lightgreen', 'lightgrey', 'lightpink', 'lightpurple', 'lightorange', 'lightbrown', 'lightred', 'lightyellow','darkslategray', 'darkolivegreen', 'darkorchid', 'darkorange', 'darkmagenta', 'darkkhaki', 'darkgreen', 'darkgoldenrod', 'darkcyan', 'darkblue', 'darkred', 'darkviolet', 'darkturquoise', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey']
+
+// let intervalId = setInterval(() => {
+//     let randomColor = color[Math.floor(Math.random() * color.length)]
+//     main.style.backgroundColor = randomColor
+//     div.style.backgroundColor = randomColor
+//     body.style.backgroundColor = randomColor
+// }, 3000)
+
+// setTimeout(() => clearInterval(intervalId), 10000)
